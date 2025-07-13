995ab2b76708f5c383f7c5a75b2cea7c269264c8
razor <shahmusharaf1128@gmail.com>
2025-07-13 18:43:59 +0530

    new changes

--- app/main.py
+++ app/main.py
@@ -1,15 +1,25 @@
 from fastapi import FastAPI
 from app.database import init_db
 from app.routers import users, topics, sections, progress, tutor
+from fastapi.middleware.cors import CORSMiddleware
 
-app = FastAPI(title="learnX-backend")
+app = FastAPI()
 
+# localhost POST access granted
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# initialise database on app startup
 @app.on_event("startup")
 def on_startup():
     init_db()
 
 # Include API routers
-# app.include_router(auth.router, prefix="/auth", tags=["Auth"])
 app.include_router(users.router, prefix="/users", tags=["Users"])
 app.include_router(topics.router, prefix="/topics", tags=["Topics"])
 app.include_router(sections.router, prefix="/sections", tags=["Sections"])
